60a96c1054004665040d94ff516bb3d53d1caad9
protap9ghosh <protapg518@gmail.com>
2023-02-07 18:45:14 +0600

    cheage third problem

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function mindGame(number) {
 
     return subNumber;
 }
-const inputNumber = 33;
+const inputNumber = 15;
 const output = mindGame(inputNumber);
 console.log(output);
 
@@ -21,7 +21,7 @@ function evenOdd(inputString) {
     if(countNumber % 2 == 0) {
         return "even";
     }
-    else if(countNumber % 2 == 1) {
+    else if(countNumber % 2 !== 0) {
         return "odd";
     }
     else {
@@ -29,7 +29,7 @@ function evenOdd(inputString) {
     }
 
 }
-let value = "chatgpt";
+let value = "phero";
 let outputValue = evenOdd(value);
 console.log(outputValue);
 
@@ -42,8 +42,9 @@ console.log(outputValue);
 // Find the difference between small and large numbers
 function isLGSeven(numberValue) {
     const lessThan = 7;
-    if(numberValue <= lessThan) {
-        return numberValue;
+    if(numberValue < lessThan) {
+        const subtractionValue = numberValue - lessThan;
+        return subtractionValue;
     }
     else if(numberValue >= lessThan) {
         const greaterThan = numberValue * 2;
@@ -53,7 +54,7 @@ function isLGSeven(numberValue) {
         return "Please, input number value"
     }
 }
-const visual = 8;
+const visual = -15;
 const differenceValue = isLGSeven(visual);
 console.log(differenceValue);
 
@@ -66,5 +67,37 @@ console.log(differenceValue);
 
 
 // check positive and negative numbers in the function
+/* function findingBadData(datas) {
+    if(datas.length >= 0) {
+        const goodData = datas.length;
+        return goodData;
+    }
+    else if(datas.length <= 0) {
+        const badData = datas.length;
+        return badData;
+    }
+    else {
+        console.log("Try agin, Please input a array value")
+    }
+}
+const datas = [-12, 34, -6, 678, -89];
+const outputArray = findingBadData(datas);
+console.log(outputArray);
+*/
+
+
+
+
+/* var arrayNumbers = [23, 34, 45, 56, 67, 39, 78, 98];
+for(var i = 0; i < 7; i++) {
+    valueNumber = arrayNumbers[i];
+    if(valueNumber > 50) {
+        continue;
+    }
+    console.log(valueNumber);
+} 
+*/
+
+
 
 
